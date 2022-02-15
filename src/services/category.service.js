a8ba055519bda1811fbc1594b010a684b3309cba
river248 <85404917+river248@users.noreply.github.com>
2022-02-15 21:20:08 +0700

    Firebase.

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,10 +1,17 @@
+import firebaseApp from '../config/firebaseConfig'
 import { categoryModel } from '../models/category.model'
+import { ref, getDownloadURL, getStorage } from 'firebase/storage'
 import { HttpStatusCode } from '../utils/constants'
 
-const getAllCategories = async (req, res) => {
+const getAllCategories = async () => {
     try {
-        const result = await categoryModel.getAllCategories()
-
+        firebaseApp()
+        const categories = await categoryModel.getAllCategories()
+        const storage = getStorage()
+        const result = categories.map((category) => {
+            const gsReference = ref(storage, `gs://bucket/${category.thumbnail}`)
+            console.log(gsReference)
+        })
         return result
     } catch (error) {
         throw new Error(error)
